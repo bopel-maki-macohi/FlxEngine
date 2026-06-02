c847306b2f13e8803d66f1dcf2e76625d15b99d8
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-02 11:46:30 -0500

    Prevent FlxVersion from displaying FlxEngine Git SHA

---
+++ source/flixel/system/FlxVersion.hx
@@ -0,0 +1,28 @@
+package flixel.system;
+
+/**
+ * Helper object for semantic versioning.
+ * @see   http://semver.org/
+ */
+class FlxVersion
+{
+	public var major(default, null):Int;
+	public var minor(default, null):Int;
+	public var patch(default, null):Int;
+
+	public function new(Major:Int, Minor:Int, Patch:Int)
+	{
+		major = Major;
+		minor = Minor;
+		patch = Patch;
+	}
+
+	/**
+	 * Formats the version in the format "HaxeFlixel MAJOR.MINOR.PATCH",
+	 * e.g. HaxeFlixel 3.0.4.
+	 */
+	public function toString():String
+	{
+		return 'HaxeFlixel $major.$minor.$patch';
+	}
+}
