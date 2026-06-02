8d11e384379e3ee37bc1ad312837f721c1c0293b
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-02 11:47:55 -0500

    missing sha field

--- source/flixel/system/FlxVersion.hx
+++ source/flixel/system/FlxVersion.hx
@@ -6,6 +6,8 @@ package flixel.system;
  */
 class FlxVersion
 {
+    public static final sha:String = '';
+
 	public var major(default, null):Int;
 	public var minor(default, null):Int;
 	public var patch(default, null):Int;
