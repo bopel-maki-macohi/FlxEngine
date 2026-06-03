7140405f24a4706726b62cac34541fbbd9ff5cdc
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-02 23:55:24 -0500

    I want to fix the black bars but I dont know howwwwwwwwwww

--- source/flixel/engine/InitState.hx
+++ source/flixel/engine/InitState.hx
@@ -5,6 +5,8 @@ import flixel.addons.transition.TransitionData;
 import flixel.engine.editor.EditorState;
 import flixel.engine.util.FileUtil;
 import flixel.math.FlxPoint;
+import flixel.system.scaleModes.FixedScaleAdjustSizeScaleMode;
+import flixel.system.scaleModes.RelativeScaleMode;
 import flixel.util.FlxColor;
 
 class InitState extends FlxState
@@ -17,6 +19,9 @@ class InitState extends FlxState
 
 		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 1, FlxPoint.weak(0, -1));
 		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 1, FlxPoint.weak(0, 1));
+
+		// FlxG.scaleMode = new RelativeScaleMode(1, 1);
+		// FlxG.scaleMode = new FixedScaleAdjustSizeScaleMode(true, true,);
 
 		FlxG.switchState(() -> new EditorState());
 	}
